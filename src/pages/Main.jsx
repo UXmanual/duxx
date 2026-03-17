5845266d269ac06250b1b8d10f3955f10bd714fc
uxmanual <rich@example.com>
2026-03-17 10:02:18 +0900

    v24.1: 실시간 날씨 위젯 및 버전 동기화 완료

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -18,7 +18,7 @@ const formatDateTime = (dateString) => {
 };
 /**
  * [Page] 메인 페이지 (지도 메모 기능 통합 버전)
- * @version 24.0
+ * @version 24.1
  * @author Antigravity
  * @description 
  * - 바블 리스트 간격 복구 및 닉네임 하단 여백 최적화 버전입니다.
